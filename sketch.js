f8ec0404da1d5b02a26d170bb31668a6cf215aed
KavinSharon <69906375+KavinSharon@users.noreply.github.com>
2020-09-23 19:26:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+var bobObject1,bobObject2,bobObject3,bobObject4,bobObject5;
+var roof;
+var bobDiameter;
+var rope1,rope2,rope3,rope4,rope5;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+    bobDiameter = 20;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+     bobObject1 = new Bob(380,300,10);
+	 bobObject2 = new Bob(390,300,10);
+	 bobObject3 = new Bob(400,300,10);
+	 bobObject4 = new Bob(410,300,10);
+	 bobObject5 = new Bob(420,300,10);
+	 roof = new Roof(400,100,200,20);
+	 rope1 = new rope(bobObject1.body,roof.body,-bobDiameter*2,0);
+	 rope2 = new rope(bobObject2.body,roof.body,-bobDiameter*1,0);
+	 rope3 = new rope(bobObject3.body,roof.body,0,0);
+	 rope4 = new rope(bobObject4.body,roof.body,bobDiameter*1,0);
+	 rope5 = new rope(bobObject5.body,roof.body,bobDiameter*2,0);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+  	bobObject1.display();
+	bobObject2.display();
+	bobObject3.display();
+	bobObject4.display();
+	bobObject5.display();
+    roof.display();
+	rope1.display();
+	rope2.display();
+	rope3.display();
+	rope4.display();
+	rope5.display();
+}
+
+
+
